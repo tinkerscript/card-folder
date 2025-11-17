fac117da932cd53db07b587945d75890833af062
tinkerscript <tinkerscript@users.noreply.github.com>
2025-11-17 13:21:05 +0000

    deploy: a7ab7598fa3bc68f2ddc15d4dbbfb508bd366c36

--- scripts/generate_cards.rb
+++ scripts/generate_cards.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 require "yaml"
 require "fileutils"
 
@@ -10,19 +11,21 @@ cards.each do |entry|
   file, meta = entry.first
 
   html = <<~HTML
-    ---
-    layout: card
-    title: "#{meta["title"]}"
-    file: "#{file}"
-    suit: "#{meta["suit"]}"
-    group: "#{meta["group"]}"
-    rarity: "#{meta["rarity"]}"
-    description: "#{meta["description"]}"
-    permalink: "/cards/#{file.sub('.webp','')}.html"
-    ---
+  ---
+  layout: card
+  title: "#{meta['title']}"
+  ---
+
+  <div class="card-page">
+    <img class="card-image" src="{{ site.baseurl }}/assets/cards/#{file }}" alt="#{meta['description']}">
+    <div class="card-info">
+      <p><strong>Suit:</strong> #{data["suits"][meta["suit"]]}</p>
+      <p><strong>Group:</strong> #{data["groups"][meta["group"]]}</p>
+      <p><strong>Rarity:</strong> #{data["rarity"][meta["rarity"]]}</p>
+      <p><strong>Description:</strong> #{meta['description']}</p>
+    </div>
+  </div>
   HTML
 
-  File.write("cards/#{file.sub('.webp','')}.html", html)
+  File.write("cards/#{file.sub('.webp', '.html')}", html)
 end
-
-puts "Generated #{cards.length} card pages."
